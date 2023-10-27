fbde40409b8e3f6e6a788babc8b7ab117dafe6e1
limengzhe <li.mengzhe@outlook.com>
2023-10-27 14:25:16 +0800

    build: 删除 prettier 配置和相关依赖

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [require('prettier-plugin-tailwindcss')],
-  tailwindConfig: 'tailwind.config.js',
-  semi: false,
-  singleQuote: true,
-  arrowParens: 'avoid',
-  printWidth: 140,
-}
