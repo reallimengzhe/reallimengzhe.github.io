911610646d8245d1e26b26d71ec8f9234a72df1a
limengzhe <li.mengzhe@outlook.com>
2023-10-27 14:26:01 +0800

    feat: 增加搜索引擎抓取配置

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,9 @@
 import '@/styles/globals.css'
-import '@/styles/theme-switcher.css'
 
 import '@arco-design/web-react/dist/css/arco.css'
 
 import Head from 'next/head'
 import NavBar from '@/components/nav-bar'
-import LayoutHeader from '@/components/layout-header'
 import LayoutFooter from '@/components/layout-footer'
 
 export default function App({ Component, pageProps }) {
@@ -13,11 +11,11 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>李孟哲的主页</title>
-        <meta name="description" content="李孟哲的个人博客" />
-        <meta name="keywords" content="李孟哲, li mengzhe, limengzhe, 前端开发, Web develop" />
+        <meta name='keywords' content='李孟哲, li mengzhe, limengzhe, 前端开发, Web develop' />
+        <meta name='description' content='李孟哲的个人博客' />
+        <meta name='robots' content='index, follow' />
       </Head>
       <NavBar />
-      <LayoutHeader />
       <Component {...pageProps} />
       <LayoutFooter />
     </>
