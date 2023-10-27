2abbef25e4bb4bc0581568851f270ad05ea9722f
limengzhe <li.mengzhe@outlook.com>
2023-10-27 14:28:54 +0800

    feat: 简化 导航条 CSS

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -8,17 +8,27 @@ export default function NavBar() {
   const { pathname } = useRouter()
 
   return (
-    <div className="nav_bar fixed left-0 right-0 top-0 z-50 h-16 w-full border-b px-4 backdrop-blur">
-      <div className="nav_bar-main mx-auto flex h-full max-w-7xl items-center">
-        <Link href="/" className="nav_bar-logo flex h-7 w-auto items-center rounded-full text-xs">
+    <div className='nav_bar sticky left-0 right-0 top-0 z-50 h-20 w-full border-b px-4 backdrop-blur bg-white/70 dark:bg-slate-950/70 '>
+      <div className='nav_bar-main mx-auto flex h-full max-w-7xl items-center'>
+        <Link href='/' className='nav_bar-logo flex h-7 w-auto items-center rounded-full text-xs'>
           <span>首页</span>
         </Link>
 
-        <Link href="/project" className={`nav_bar-item ml-auto rounded-full px-4 py-1 text-xs ${pathname === '/project' ? 'is-active' : ''}`}>
-          <span>项目</span>
+        <Link
+          href='/project'
+          className={`nav_bar-item ml-auto rounded-md px-4 py-1 text-sm hover:bg-slate-100 dark:hover:bg-slate-800 ${
+            pathname === '/project' ? 'font-bold' : ''
+          }`}
+        >
+          项目
         </Link>
-        <Link href="/post" className={`nav_bar-item ml-4 rounded-full px-4 py-1 text-xs ${pathname === '/post' ? 'is-active' : ''}`}>
-          <span>文章</span>
+        <Link
+          href='/post'
+          className={`nav_bar-item ml-4 rounded-md px-4 py-1 text-sm hover:bg-slate-100 dark:hover:bg-slate-800 ${
+            pathname === '/post' ? 'font-bold' : ''
+          }`}
+        >
+          部落格
         </Link>
 
         <ThemeSwitcher />
