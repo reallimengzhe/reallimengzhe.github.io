8969b89d5bc981ab70570e67c360a6d43f360a9c
limengzhe <li.mengzhe@outlook.com>
2023-10-27 14:31:30 +0800

    feat: 调整 项目页 UI

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
-import { IconMore, IconExpand, IconClockCircle } from '@arco-design/web-react/icon'
 
 import ProjectViewer from '@/components/project-viewer'
+import IconMore from '@/components/icon-more'
 import { setAnimation } from '@/utils'
 
 export default function Project() {
@@ -17,48 +17,44 @@ export default function Project() {
   }
 
   return (
-    <main className="page_project relative px-4">
-      <div
-        className="page_project-main mx-auto grid max-w-7xl gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
-        style={{ marginTop: '-32px' }}
-      >
+    <main className='page_project relative p-4'>
+      <div className='page_project-main mx-auto grid max-w-7xl gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
         {projects
           .filter(item => item.show)
           .map(project => {
             return (
-              <div className="page_project-item" key={project.name}>
+              <div className='page_project-item' key={project.name}>
                 <div
-                  className="content-bg rounded-xl border p-4 transition duration-300 hover:-translate-y-1 hover:shadow"
+                  className='content-bg rounded-xl border p-4 transition duration-300 hover:-translate-y-1 hover:shadow'
                   key={project.name}
                   onClick={() => onViewProject(project)}
                 >
-                  <img src={project.img[0]} className="mb-4 aspect-3/2 w-full rounded-md object-cover" />
-                  <h5 className="mb-2">
+                  <img src={project.img[0]} className='mb-4 aspect-3/2 w-full rounded-md object-cover' />
+                  <h5 className='mb-2'>
                     <span>{project.name}</span>
-                    <IconExpand className="ml-2 transition-opacity duration-300" />
                   </h5>
-                  <p className="text-secondary mb-4"> {project.type} </p>
+                  <p className='text-secondary mb-4'> {project.type} </p>
                   {/* <p className="mb-4"> {project.orgination} </p> */}
-                  <div className="flex">
+                  <div className='flex'>
                     {project.techStack.map((tech, index) => {
                       return (
                         <div
-                          className="page_project-item-tech flex h-8 w-8 items-center justify-center rounded-full"
+                          className='page_project-item-tech flex h-8 w-8 items-center justify-center rounded-full'
                           style={{
                             marginLeft: index === 0 ? '-4px' : '-8px',
                             zIndex: 10 - index,
                           }}
                           key={tech}
                         >
-                          <img src={'/icon/' + tech + '.svg'} className="h-7/12 w-7/12" />
+                          <img src={'/icon/' + tech + '.svg'} className='h-7/12 w-7/12' />
                         </div>
                       )
                     })}
 
                     <div
-                      className="page_project-item-tech flex h-8 w-8 items-center justify-center rounded-full"
+                      className='page_project-item-tech flex h-8 w-8 items-center justify-center rounded-full'
                       style={{ marginLeft: '-8px' }}
-                      key="more"
+                      key='more'
                     >
                       <IconMore />
                     </div>
@@ -67,11 +63,6 @@ export default function Project() {
               </div>
             )
           })}
-        <div className="page_project-item content-bg flex flex-col items-center justify-center rounded-xl border p-4" key="wait">
-          <IconClockCircle className="mb-4 text-4xl" />
-          <h5 className="mb-2"> 更多项目 </h5>
-          <p className="text-secondary"> 敬请期待 </p>
-        </div>
       </div>
 
       <ProjectViewer ref={ProjectViewerRef} />
