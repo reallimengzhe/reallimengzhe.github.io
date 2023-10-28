da2854496f20d1397a567fe1a1c2ab6b41a9f31f
limengzhe <li.mengzhe@outlook.com>
2023-10-29 00:24:27 +0800

    feat: 项目详情页 UI 调整

--- src/pages/projects/[id].js
+++ src/pages/projects/[id].js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-import IconArrowLeft from '@/components/icon-arrow-left'
+import IconArrowBack from '@/components/icon-arrow-back'
 import IconMore from '@/components/icon-more'
 
 export default function Project() {
@@ -14,30 +14,43 @@ export default function Project() {
 
   return (
     project && (
-      <main className='p-4 pt-8'>
-        <div className='mx-auto max-w-7xl pb-10 mb-12 border-b border-slate-200 dark:border-slate-800'>
-          <Link href='javascript:history.back(-1)' className='text-xl flex items-center gap-2 hover:text-slate-400 transition-colors mb-8'>
-            <IconArrowLeft />
+      <main className='px-6 py-10'>
+        <div className='mx-auto max-w-7xl pb-10 mb-10 border-b border-slate-200 dark:border-slate-800'>
+          <Link
+            href='javascript:history.back(-1)'
+            className='flex items-center gap-2 text-slate-400 hover:text-slate-600 transition-colors mb-10 w-fit'
+          >
+            <IconArrowBack />
             <span>返回</span>
           </Link>
 
-          <div className='flex gap-2 flex-wrap mb-4 text-klein-600 dark:text-cyan-700'>
+          <div className='flex gap-2 flex-wrap mb-4 text-klein-600 dark:text-cyan-600'>
             {project.keywords.map(keyword => {
               return <span key={keyword}>#{keyword}</span>
             })}
           </div>
 
-          <h1 className='text-5xl font-extrabold'>{project.name}</h1>
+          <h1 className='text-5xl font-extrabold mb-8'>{project.name}</h1>
 
           <p className='text-lg text-slate-400'>{project.intro}</p>
         </div>
 
         <div className='mx-auto max-w-7xl'>
-          <img src={project.img[0]} className='rounded-xl mb-8'></img>
+          <h5 className='text-slate-400 text-lg mb-4'>页面截图</h5>
 
-          <h5 className='text-slate-400 text-lg font-bold mb-4'>技术栈</h5>
+          <img src={project.img[0]} className='border border-slate-200 dark:border-slate-800 w-full rounded-xl mb-8'></img>
 
-          <div className='flex items-center mt-auto mb-8'>
+          <h5 className='text-slate-400 text-lg mb-4'>职责或成果</h5>
+
+          <ul className='grid gap-2 mb-8'>
+            {project.duty.map(duty => {
+              return <li key={duty}>{duty}</li>
+            })}
+          </ul>
+
+          <h5 className='text-slate-400 text-lg mb-4'>技术架构</h5>
+
+          <div className='flex items-center mt-auto'>
             {project.techStack.map((tech, index) => {
               return (
                 <div
@@ -60,17 +73,6 @@ export default function Project() {
               <IconMore />
             </div>
           </div>
-
-          <h5 className='text-slate-400 text-lg font-bold mb-4'>我的职责</h5>
-
-          <ul className='grid gap-2 mb-8'>
-            {project.duty.map(duty => {
-              return <li key={duty}>{duty}</li>
-            })}
-          </ul>
-
-          <h5 className='text-slate-400 text-lg font-bold mb-4'>项目成果</h5>
-          <p className='text-justify'>{project.gains}</p>
         </div>
       </main>
     )
