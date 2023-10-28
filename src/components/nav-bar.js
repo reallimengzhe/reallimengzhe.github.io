1a8b1086bffaa1f344d6d37b9c423af4453b3ecb
limengzhe <li.mengzhe@outlook.com>
2023-10-29 00:26:32 +0800

    feat: 支持从首页、导航条跳转至 简历 页

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -8,15 +8,15 @@ export default function NavBar() {
   const { pathname } = useRouter()
 
   return (
-    <div className='nav_bar sticky left-0 right-0 top-0 z-50 h-20 w-full border-b border-slate-200 dark:border-slate-800 px-4 backdrop-blur bg-white/70 dark:bg-slate-950/70 '>
-      <div className='nav_bar-main mx-auto flex h-full max-w-7xl items-center'>
+    <div className='nav_bar sticky top-0 z-50 w-full border-b border-slate-200 dark:border-slate-800 px-6 backdrop-blur bg-white/70 dark:bg-slate-950/70'>
+      <main className='max-w-7xl h-20 mx-auto flex items-center gap-4'>
         <Link href='/' className='nav_bar-logo flex h-7 w-auto items-center rounded-full text-xs'>
           <span>首页</span>
         </Link>
 
         <Link
           href='/projects'
-          className={`nav_bar-item ml-auto rounded-md px-4 py-1 text-sm hover:bg-slate-100 dark:hover:bg-slate-800 ${
+          className={`ml-auto rounded-md px-2 py-1 text-sm hover:bg-slate-100 dark:hover:bg-slate-900 ${
             pathname === '/projects' ? 'font-bold' : ''
           }`}
         >
@@ -24,15 +24,23 @@ export default function NavBar() {
         </Link>
         <Link
           href='/post'
-          className={`nav_bar-item ml-4 rounded-md px-4 py-1 text-sm hover:bg-slate-100 dark:hover:bg-slate-800 ${
+          className={`rounded-md px-2 py-1 text-sm hover:bg-slate-100 dark:hover:bg-slate-900 ${
             pathname === '/post' ? 'font-bold' : ''
           }`}
         >
           部落格
         </Link>
+        <Link
+          href='/resume'
+          className={`rounded-md px-2 py-1 text-sm hover:bg-slate-100 dark:hover:bg-slate-900 mr-2 ${
+            pathname === '/resume' ? 'font-bold' : ''
+          }`}
+        >
+          简历
+        </Link>
 
         <ThemeSwitcher />
-      </div>
+      </main>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -59,135 +59,144 @@ export default function Home() {
 
   return (
     <>
-      <header className='home-header relative overflow-hidden px-4 pb-32 pt-40 md:pb-32 md:pt-32 mx-auto bg-bottom border-b border-slate-200 dark:border-slate-800'>
+      <header className='home-header relative overflow-hidden px-6 py-32 md:py-48 bg-bottom border-b border-slate-200 dark:border-slate-800'>
         {/* <div ref={vantaRef} className='absolute bottom-0 left-0 right-0 top-0'></div> */}
 
-        <h1 className='text-center font-extrabold md:text-6xl'>有朋自远方来，不亦乐乎。 🎈</h1>
-        {/* <img src="/avatar.png" className="relative z-10 mx-auto mb-4 h-20 w-20 rounded-full" draggable={false} /> */}
+        <h1 className='text-center font-extrabold text-4xl md:text-6xl mb-12'>有朋自远方来，不亦乐乎。 🎈</h1>
 
-        <p className='text-center text-lg mx-auto mb-20 text-slate-400 max-w-5xl leading-8'>
-          你好！我是李孟哲，欢迎访问我的主页。作为一名前端开发工程师，我专注于构建 <span className='font-bold text-klein-700'>界面友好</span> 和{' '}
-          <span className='font-bold text-klein-700'>用户体验友好</span> 的前端应用程序。技术很重要，使用技术的人更重要。
+        <p className='text-center text-lg mx-auto mb-20 text-slate-600 max-w-5xl leading-8'>
+          你好！我是李孟哲，欢迎访问我的主页。作为一名前端开发工程师，我专注于构建{' '}
+          <span className='font-bold text-klein-600 dark:text-cyan-600'>界面友好</span> 和{' '}
+          <span className='font-bold text-klein-600 dark:text-cyan-600'>用户体验友好</span>{' '}
+          的前端应用程序。对于开发这件事，技术很重要，使用技术的人更重要。
         </p>
 
-        <button
-          className='mx-auto mb-20 bg-cyan-600 hover:bg-cyan-200 text-black dark:text-white font-semibold h-12 rounded-lg w-40 flex items-center justify-center dark:bg-klein-500 dark:highlight-white/20 dark:hover:bg-klein-400'
-          onClick={() => {
-            document.getElementById('main-content').scrollIntoView({ behavior: 'smooth' })
-          }}
-        >
-          <span className='mr-2'>开始了解</span>
-          <IconArrowDown />
-        </button>
+        <div className='flex justify-center items-center gap-6'>
+          <button
+            className='bg-klein-600 hover:bg-klein-400 dark:bg-cyan-600 dark:hover:bg-cyan-700 text-white dark:text-black transition-colors font-bold text-sm rounded-lg flex items-center px-6 py-3'
+            onClick={() => {
+              document.getElementById('main-content').scrollIntoView({ behavior: 'smooth' })
+            }}
+          >
+            <span className='mr-2'>详细了解</span>
+            <IconArrowDown />
+          </button>
+
+          <Link
+            href='/resume'
+            className='bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-900 text-black dark:text-white transition-colors font-bold text-sm rounded-lg flex items-center px-6 py-3'
+          >
+            <span className='mr-2'>与我联系</span>
+            <IconArrowRight />
+          </Link>
+        </div>
       </header>
+      <div className='px-6'>
+        <main className='home relative max-w-7xl py-32 mx-auto' id='main-content'>
+          <h2 className='font-bold text-2xl md:text-4xl mb-10 text-center home-module'>💎</h2>
+          <h2 className='font-bold text-2xl md:text-4xl mb-20 text-center home-module'>我曾构建过多个大型项目，这对我来说游刃有余。</h2>
 
-      <main className='home relative max-w-7xl mx-auto'>
-        <h2 className='font-bold text-4xl mb-10 text-center pt-32 home-module' id='main-content'>
-          💎
-        </h2>
-        <h2 className='font-bold text-4xl mb-20 text-center home-module'>我曾构建过多个大型项目，这对我来说游刃有余。</h2>
+          <div className='grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-16 home-module'>
+            {projects
+              .filter(item => item.show)
+              .slice(0, 3)
+              .map(project => {
+                return (
+                  <Link
+                    key={project.name}
+                    className='flex flex-col items-start rounded-xl border border-slate-200 dark:border-slate-800 p-7 transition-all hover:-translate-y-1 hover:shadow'
+                    href={'/projects/' + project.id}
+                  >
+                    <img src={project.logo} className='h-10 mb-16' />
 
-        <div className='grid gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-16 home-module'>
-          {projects
-            .filter(item => item.show)
-            .slice(0, 3)
-            .map(project => {
-              return (
-                <Link
-                  key={project.name}
-                  className='flex flex-col items-start rounded-xl border border-slate-200 dark:border-slate-800 p-7 transition-all hover:-translate-y-1 hover:shadow'
-                  href={'/projects/' + project.id}
-                >
-                  <img src={project.logo} className='h-10 mb-16' />
+                    <h5 className='text-xl font-bold mb-2'>{project.name}</h5>
 
-                  <h5 className='text-xl font-bold mb-2'>{project.name}</h5>
-
-                  <div className='flex gap-2 flex-wrap mb-4 text-klein-600 dark:text-cyan-700'>
-                    {project.keywords.map(keyword => {
-                      return <span key={keyword}>#{keyword}</span>
-                    })}
-                  </div>
-
-                  <p className='text-slate-400 mb-4'>{project.intro}</p>
-
-                  <div className='flex items-center mt-auto'>
-                    {project.techStack.map((tech, index) => {
-                      return (
-                        <div
-                          key={tech}
-                          className='flex h-8 w-8 items-center justify-center rounded-full bg-slate-100 dark:bg-slate-800 border-4 border-white dark:border-slate-950'
-                          style={{
-                            marginLeft: index == 0 ? '-4px' : '-8px',
-                            zIndex: 10 - index,
-                          }}
-                        >
-                          <img src={'/icon/' + tech + '.svg'} className='h-7/12 w-7/12' />
-                        </div>
-                      )
-                    })}
-
-                    <div
-                      className='flex h-8 w-8 items-center justify-center rounded-full bg-slate-100 dark:bg-slate-800 border-4 border-white dark:border-slate-950'
-                      style={{ marginLeft: '-8px' }}
-                    >
-                      <IconMore />
+                    <div className='flex gap-2 flex-wrap mb-4 text-klein-600 dark:text-cyan-600'>
+                      {project.keywords.map(keyword => {
+                        return <span key={keyword}>#{keyword}</span>
+                      })}
                     </div>
+
+                    <p className='text-slate-400 mb-4'>{project.intro}</p>
+
+                    <div className='flex items-center mt-auto'>
+                      {project.techStack.map((tech, index) => {
+                        return (
+                          <div
+                            key={tech}
+                            className='flex h-8 w-8 items-center justify-center rounded-full bg-slate-100 dark:bg-slate-800 border-4 border-white dark:border-slate-950'
+                            style={{
+                              marginLeft: index == 0 ? '-4px' : '-8px',
+                              zIndex: 10 - index,
+                            }}
+                          >
+                            <img src={'/icon/' + tech + '.svg'} className='h-7/12 w-7/12' />
+                          </div>
+                        )
+                      })}
+
+                      <div
+                        className='flex h-8 w-8 items-center justify-center rounded-full bg-slate-100 dark:bg-slate-800 border-4 border-white dark:border-slate-950'
+                        style={{ marginLeft: '-8px' }}
+                      >
+                        <IconMore />
+                      </div>
+                    </div>
+                  </Link>
+                )
+              })}
+          </div>
+
+          <Link
+            href='/projects'
+            className='flex items-center justify-center gap-1 mx-auto w-fit px-6 h-12 hover:bg-slate-50 hover:dark:bg-slate-900 rounded-lg transition-colors home-module mb-20'
+          >
+            <span> 查看更多 </span>
+            <IconArrowRight />
+          </Link>
+
+          <h2 className='font-bold text-2xl md:text-4xl mb-10 home-module'>📢</h2>
+          <h2 className='font-bold text-2xl md:text-4xl mb-20 home-module'>知识不应该被垄断，分享也是学习的一部分。</h2>
+
+          <div className='home-post-content grid gap-8 mb-16'>
+            {posts.slice(0, 5).map(post => {
+              return (
+                <div className='flex home-module' key={post.title}>
+                  <img src={'/avatar.png'} className='h-16 w-16 rounded-full mr-8 mt-2' />
+                  <div className='home-post-item block flex-1 rounded-xl border border-slate-200 dark:border-slate-800 p-4 transition-all hover:-translate-y-1 hover:shadow'>
+                    <Link href={post.link} target='_blank' className={`block text-2xl font-bold mb-4`}>
+                      <span>{post.title}</span>
+                    </Link>
+
+                    <div className='flex gap-4 flex-wrap mb-8 text-klein-600 dark:text-cyan-600'>
+                      {post.keywords.map(keyword => {
+                        return <span key={keyword}>#{keyword}</span>
+                      })}
+                    </div>
+
+                    {/* <p className='text-slate-400'> {post.time} </p> */}
+
+                    <p className='text-slate-400 mb-4'>{post.abstract}</p>
+
+                    <Link href={post.link} target='_blank' className='text-klein-600 dark:text-cyan-600 flex items-center'>
+                      <span> 阅读全文 </span>
+                      <IconArrowRight className='ml-1' />
+                    </Link>
                   </div>
-                </Link>
+                </div>
               )
             })}
-        </div>
+          </div>
 
-        <Link
-          href='/projects'
-          className='flex items-center justify-center mb-20 mx-auto w-32 hover:bg-slate-100 hover:dark:bg-slate-800 py-2 rounded-lg transition-colors home-module'
-        >
-          <span> 查看更多 </span>
-          <IconArrowRight className='ml-1' />
-        </Link>
+          <Link
+            href='/post'
+            className='flex items-center justify-center gap-1 mx-auto w-fit px-6 h-12 hover:bg-slate-50 hover:dark:bg-slate-900 rounded-lg transition-colors home-module'
+          >
+            <span> 查看更多 </span>
+            <IconArrowRight />
+          </Link>
 
-        <h2 className='font-bold text-4xl mb-10 home-module'>📢</h2>
-        <h2 className='font-bold text-4xl mb-20 home-module'>知识不应该被垄断，分享也是学习的一部分。</h2>
-
-        <div className='home-post-content grid gap-8 mb-20'>
-          {posts.slice(0, 5).map(post => {
-            return (
-              <div className='flex home-module' key={post.title}>
-                <img src={'/avatar.png'} className='h-16 w-16 rounded-full mr-8 mt-2' />
-                <div className='home-post-item block flex-1 rounded-xl border border-slate-200 dark:border-slate-800 p-4 transition-all hover:-translate-y-1 hover:shadow'>
-                  <Link href={post.link} target='_blank' className={`block text-2xl font-bold mb-4`}>
-                    <span>{post.title}</span>
-                  </Link>
-
-                  <div className='flex gap-4 flex-wrap mb-8 text-klein-600 dark:text-cyan-700'>
-                    {post.keywords.map(keyword => {
-                      return <span key={keyword}>#{keyword}</span>
-                    })}
-                  </div>
-
-                  {/* <p className='text-slate-400'> {post.time} </p> */}
-
-                  <p className='text-slate-400 mb-4'>{post.abstract}</p>
-
-                  <Link href={post.link} target='_blank' className='text-klein-600 dark:text-cyan-700 flex items-center'>
-                    <span> 阅读全文 </span>
-                    <IconArrowRight className='ml-1' />
-                  </Link>
-                </div>
-              </div>
-            )
-          })}
-        </div>
-
-        <Link
-          href='/post'
-          className='flex items-center justify-center mb-20 mx-auto w-32 hover:bg-slate-100 hover:dark:bg-slate-800 py-2 rounded-lg transition-colors home-module'
-        >
-          <span> 查看更多 </span>
-          <IconArrowRight className='ml-1' />
-        </Link>
-
-        {/* <div className='px-4 py-4'>
+          {/* <div className='px-6 py-4'>
           <div className='mb-4 flex'>
             <Link
               href='https://www.zzuli.edu.cn/'
@@ -216,7 +225,8 @@ export default function Home() {
             </div>
           </div>
         </div> */}
-      </main>
+        </main>
+      </div>
     </>
   )
 }
