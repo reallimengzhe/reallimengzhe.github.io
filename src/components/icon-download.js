62c32ee296b0e0ba4b2b781846fcb7db095ce631
limengzhe <li.mengzhe@outlook.com>
2024-12-25 10:21:53 +0800

    feat: 更新简历

--- src/components/icon-download.js
+++ src/components/icon-download.js
@@ -1,7 +1,7 @@
 export default function ({ fill = 'currentColor', width = '1em', height = '1em', className = '', style = {} }) {
   return (
     <svg width={width} height={height} viewBox='0 0 24 24' fill={fill} xmlns='http://www.w3.org/2000/svg' className={className} style={style}>
-      <path d="M7.8,13l5.4,5.4-1.4,1.4-7.8-7.8,7.8-7.8,1.4,1.4-5.4,5.4h12.2v2H7.8Z"/>
+      <path d="M5.4,9.4l1.4-1.4,4.7,4.7V3.5s2,0,2,0v9.2s4.7-4.7,4.7-4.7l1.4,1.4-7.1,7.1-7.1-7.1ZM19.5,20.5H5.5v-2h14v2Z"/>
     </svg>
   )
 }
--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,4 +1,4 @@
-import IconArrowDown from '@/components/icon-arrow-down'
+import IconDownload from '@/components/icon-download'
 
 import printJS from '@/utils/print.min.js'
 
@@ -48,9 +48,8 @@ export default function PageResume() {
           <section className='grid gap-2'>
             <h2 className='text-center text-2xl font-bold'>李孟哲</h2>
 
-            <p className='flex gap-8 justify-center'>
-              <a href='tel:+8615538077068'>155 3807 7068</a>
-              <a href='mailto:li.mengzhe@outlook.com'>li.mengzhe@outlook.com</a>
+            <p className='text-center'>
+              <a href='tel:+8615538077068'>155 3807 7068</a> & <a href='mailto:li.mengzhe@outlook.com'>li.mengzhe@outlook.com</a>
             </p>
 
             <p className='text-center'>&lt;前端开发工程师 /&gt;</p>
@@ -81,12 +80,35 @@ export default function PageResume() {
             </tbody>
           </table>
 
+          <h3 className='text-xl font-bold border-b-2 pb-2 border-slate-100 dark:border-slate-900'>专业技能</h3>
+
+          <section className='grid gap-2'>
+            <h4 className='font-bold dark:border-slate-900'>前端基础</h4>
+
+            <ul className='list-inside list-disc'>
+              <li>熟练使用 Canvas、WebSocket 等 HTML5 新特性。</li>
+              <li>熟练使用 Flex、Grid 等 CSS3 新特性，遵循 BEM 命名规范。</li>
+              <li>熟练使用 async/await、Set、Map 等 JavaScript ES6+ 新特性，能够在项目中广泛应用。</li>
+            </ul>
+          </section>
+          <section className='grid gap-2'>
+            <h4 className='font-bold dark:border-slate-900'>框架与其它</h4>
+            <ul className='list-inside list-disc'>
+              <li>熟练开发基于 Vue 2.x、Vue 3.x 的应用，理解其原理。</li>
+              <li>熟悉 React，有实际项目开发经验。</li>
+              <li>熟练使用 Webpack 和 Vite 等构建工具，有前端性能优化的具体实践。</li>
+              <li>熟悉 Node.js、Electron.js、Uni-App 和微信小程序，可以独立开发服务端应用、桌面端应用、APP 和小程序。</li>
+              <li>熟练使用 Git、Jira 等项目协同工具，保证团队协作效率。</li>
+              <li>熟练使用 Photoshop、Illustrator、Figma 等设计软件。 </li>
+            </ul>
+          </section>
+
           <h3 className='text-xl font-bold border-b-2 pb-2 border-slate-100 dark:border-slate-900'>时间线</h3>
 
           <section className='grid gap-2'>
-            <h4 className='font-bold dark:border-slate-900'>上海喜树信息科技有限公司 · 前端开发工程师 · 研发部</h4>
+            <h4 className='font-bold dark:border-slate-900'>上海喜树信息科技有限公司 · 研发部 · 前端开发工程师</h4>
 
-            <i className='text-slate-400'>2023 年 8 月 - 至今</i>
+            <i className='text-slate-400'>2023 年 8 月 - 2024 年 1 月</i>
 
             <ul className='list-inside list-disc grid gap-1'>
               <li>负责公司所有产品的前端开发工作。</li>
@@ -107,7 +129,7 @@ export default function PageResume() {
           </section>
 
           <section className='grid gap-2'>
-            <h4 className='font-bold dark:border-slate-900'>上海商汤智能科技有限公司 · 前端开发工程师 · 智慧城市与商业事业群</h4>
+            <h4 className='font-bold dark:border-slate-900'>上海商汤智能科技有限公司 · 智慧城市与商业事业群 · 前端开发工程师</h4>
 
             <i className='text-slate-400'>2020 年 10 月 - 2022 年 2 月</i>
 
@@ -122,7 +144,7 @@ export default function PageResume() {
           </section>
 
           <section className='grid gap-2'>
-            <h4 className='font-bold dark:border-slate-900'>上海树维信息科技有限公司 · 前端开发工程师 · 平台产品研发部</h4>
+            <h4 className='font-bold dark:border-slate-900'>上海树维信息科技有限公司 · 平台产品研发部 · 前端开发工程师</h4>
 
             <i className='text-slate-400'>2019 年 5 月 - 2020 年 10 月</i>
 
@@ -148,29 +170,6 @@ export default function PageResume() {
             </ul>
           </section>
 
-          <h3 className='text-xl font-bold border-b-2 pb-2 border-slate-100 dark:border-slate-900'>专业技能</h3>
-
-          <section className='grid gap-2'>
-            <h4 className='font-bold dark:border-slate-900'>前端基础</h4>
-
-            <ul className='list-inside list-disc'>
-              <li>熟练使用 Canvas、WebSocket 等 HTML5 新特性。</li>
-              <li>熟练使用 Flex、Grid 等 CSS3 新特性。CSS 命名遵循 BEM 规范。</li>
-              <li>熟练使用 async/await、Set、Map 等 JavaScript ES6+ 新特性，能够在项目中广泛且合理的运用。</li>
-            </ul>
-          </section>
-          <section className='grid gap-2'>
-            <h4 className='font-bold dark:border-slate-900'>框架与其它</h4>
-            <ul className='list-inside list-disc'>
-              <li>熟练开发基于 Vue 2.x、Vue 3.x 的应用并理解其原理。</li>
-              <li>熟悉 React，有实际项目的开发经验。</li>
-              <li>熟练使用 Webpack 和 Vite 等构建工具，有前端性能优化的具体实践。</li>
-              <li>熟悉 Node.js 、Electron.js、uni-app 和微信小程序，可以独立开发服务端应用、桌面端应用、APP 和小程序。</li>
-              <li>熟练使用 Git、Jira 等项目协同工具，保证团队协作效率。</li>
-              <li>有 UI 设计相关知识和经验，具有审美基础，且能够熟练使用 Photoshop、Illustrator、Figma 等设计软件。 </li>
-            </ul>
-          </section>
-
           <h3 className='text-xl font-bold border-b-2 pb-2 border-slate-100 dark:border-slate-900'>项目经验</h3>
 
           {projects
@@ -189,14 +188,8 @@ export default function PageResume() {
 
                   <p className='text-slate-400'>技术架构</p>
 
-                  <div className='flex gap-2'>
-                    {project.techStack.map(tech => {
-                      return (
-                        <div key={tech} className=''>
-                          {tech}
-                        </div>
-                      )
-                    })}
+                  <div className='flex gap-2' style={{ textTransform: 'capitalize' }}>
+                    {project.techStack.join(' + ')} + ···
                   </div>
 
                   <p className='text-slate-400'>职责与成果</p>
@@ -216,8 +209,8 @@ export default function PageResume() {
         className='bg-slate-100 hover:bg-slate-200 dark:bg-slate-800 dark:hover:bg-slate-900 text-black dark:text-white transition-colors text-sm rounded-lg items-center px-4 py-2 mx-auto mb-20 hidden lg:flex'
         onClick={handleExport}
       >
-        <span className='mr-2'>下载 PDF</span>
-        <IconArrowDown />
+        <span className='mr-2'>打印或另存为</span>
+        <IconDownload />
       </button>
 
       <div className='block lg:hidden text-center mb-20'>
