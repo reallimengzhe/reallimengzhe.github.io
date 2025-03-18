4606c2cb6385adba6122aa274ae14e01407e2778
limengzhe <li.mengzhe@outlook.com>
2025-03-18 18:55:30 +0800

    build: update static package

--- docs/_next/static/chunks/app/projects/[id]/page-0549db6da1b82fd8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[626],{1783:function(){}},function(n){n.O(0,[847,614,744],function(){return n(n.s=1783)}),_N_E=n.O()}]);
---
+++ docs/_next/static/chunks/app/projects/[id]/page-f5f1a2358d37fcfd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[626],{7784:function(){}},function(n){n.O(0,[847,614,744],function(){return n(n.s=7784)}),_N_E=n.O()}]);
